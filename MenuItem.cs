bb8014b577862b3d559f100a22ef590be31ecf05
anupam-cse16 <anupam.bhattacharyya.uemkcse16@gmail.com>
2020-10-30 17:13:24 +0530

    first commit

---
+++ MenuItem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PCAzureWebApi
+{
+    public class MenuItem
+    {
+        [Key]
+
+        public int Menucode { get; set; }
+        public string ItemName { get; set; }
+        public int Price { get; set; }
+        public bool active { get; set; }
+        public DateTime date_of_launch { get; set; }
+    
+        public int CategoryId { get; set; }
+        // public string CategoryName { get; set; }
+        public bool freeDelivery { get; set; }
+       
+    }
+}
